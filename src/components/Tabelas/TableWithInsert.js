c62ea84ddf658bc8da959f61fface440f3bbc2e2
Eduardo <eduardotakeuchi@outlook.com>
2022-05-08 16:09:42 -0300

    ADD forms na tabela insert

--- src/components/Tabelas/TableWithInsert.js
+++ src/components/Tabelas/TableWithInsert.js
@@ -1,4 +1,4 @@
-import { Table } from "react-bootstrap";
+import { Table, Form, Row, Col, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css"; //import css boostrap
 import React, { useState, useEffect } from "react";
 import { supabase } from "../../supabaseClient";
@@ -6,7 +6,26 @@ import { supabase } from "../../supabaseClient";
 function TableWithGet() {
   //definir estado
   const [infoTable, setTable] = useState([]);
+  const [validated, setValidated] = useState(false);
+  const [dadosForm, setInsert] = useState([]);
 
+  //
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    console.log("validated > " , validated)
+    debugger
+    console.log("dados > " , form)
+
+    
+    event.preventDefault(); //evita reload na página
+    setValidated(true);
+  };
+
+  //effect
   useEffect(() => {
     (async () => {
       let { data } = await supabase.from("financas").select("*");
@@ -15,12 +34,86 @@ function TableWithGet() {
       setTable(data);
     })();
   }, []);
+  //-------------------------------------------------------------------
 
   return (
     <div>
       {/* */}
       <p>Inserindo na tabela com informações de campos input.</p>
-      {/* */}
+      {/* FORMS */}
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Row className="mb-3">
+          {/* -------------------------------------------------------------- */}
+          <Form.Group as={Col} md="4" controlId="validationCustom01">
+            <Form.Label>Título</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Digite..."
+              /*defaultValue="Mark"*/
+            />
+            <Form.Control.Feedback>Parece bom!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              Please provide a correct information.
+            </Form.Control.Feedback>
+          </Form.Group>
+          {/* -------------------------------------------------------------- */}
+          <Form.Group as={Col} md="3" controlId="validationCustom02">
+            <Form.Label>Tipo</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Digite..."
+              /*defaultValue="Otto"*/
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              Please provide a correct information.
+            </Form.Control.Feedback>
+          </Form.Group>
+          {/* -------------------------------------------------------------- */}
+          <Form.Group as={Col} md="3" controlId="validationCustom03">
+            <Form.Label>Categoria</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Digite..."
+              /*defaultValue="Otto"*/
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              Please provide a correct information.
+            </Form.Control.Feedback>
+          </Form.Group>
+          {/* -------------------------------------------------------------- */}
+          <Form.Group as={Col} md="2" controlId="validationCustom04">
+            <Form.Label>Valor</Form.Label>
+            <Form.Control
+              required
+              type="number"
+              placeholder="Digite..."
+              /*defaultValue="Otto"*/
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              Please provide a correct information.
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        {/* -------------------------------------------------------------- */}
+        <Form.Group className="mb-3">
+          <Form.Check
+            required
+            label="Agree to terms and conditions"
+            feedback="You must agree before submitting."
+            feedbackType="invalid"
+          />
+        </Form.Group>
+        {/* -------------------------------------------------------------- */}
+        <Button type="submit">Salvar</Button>
+      </Form>
+      <br />
+      {/* TABLE */}
       <Table striped bordered hover>
         <thead>
           <tr>
